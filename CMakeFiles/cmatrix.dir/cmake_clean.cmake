0dad66a0b34503936923714331bd6baadf8956cf
Leeiss <farrakhova.2004@mail.ru>
2023-10-03 16:07:55 +0300

    third

---
+++ CMakeFiles/cmatrix.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmatrix.dir/matrixMult/func.cpp.o"
+  "CMakeFiles/cmatrix.dir/matrixMult/func.cpp.o.d"
+  "CMakeFiles/cmatrix.dir/matrixMult/main.cpp.o"
+  "CMakeFiles/cmatrix.dir/matrixMult/main.cpp.o.d"
+  "cmatrix"
+  "cmatrix.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmatrix.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
